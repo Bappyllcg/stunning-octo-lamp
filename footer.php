0f61a65793b6501203de56632625a324d3cfd26d
SA Bappy <bappy.llcg@gmail.com>
2022-01-26 02:26:07 +0600

    All File

---
+++ footer.php
@@ -0,0 +1,5 @@
+<script type='text/javascript' src='<?php echo get_template_directory_uri(); ?>/loader_boom/ajax__history.js?ver=5.3.2'></script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri(); ?>/loader_boom/ajax__loads.js?ver=5.3.2'></script>
+<script> 
+    var reload_helper = {"rootUrl":"http:\/\/<?php echo $_SERVER['HTTP_HOST']; ?>\/\/","ids":"","container_id":"main","mcdc":"menu","searchID":"searchform","transition":"","scrollTop":"","loader":"","bp_status":""}; 
+</script>
